a97e8480fc1d94c697b54f197be90f750fa80cf5
Ethan SMADJA <155528040+ethansmadjaa@users.noreply.github.com>
2026-01-26 00:27:04 +0100

    feat(supabase): add client, server, middleware utils

---
+++ src/lib/supabase/client.ts
@@ -0,0 +1,22 @@
+'use client';
+
+import { createBrowserClient } from '@supabase/ssr';
+
+let client: ReturnType<typeof createBrowserClient> | null = null;
+
+export function getSupabaseBrowserClient() {
+  if (client) {
+    return client;
+  }
+
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  if (!url || !anonKey) {
+    throw new Error('Missing environment variables!');
+  }
+
+  client = createBrowserClient(url!, anonKey!);
+
+  return client;
+}
